5782f02d2eb5c01494b96788485072a1e7a4bd47
Håkon Hægland <hakon.hagland@gmail.com>
2020-05-08 19:24:55 +0200

    Fixed compile

--- .compile.sh
+++ .compile.sh
@@ -2,7 +2,13 @@
 
 perl_include_dir=$(perl -MConfig -e'print $Config{archlib}')"/CORE"
 
-swig -w451,454 -perl5 -I/usr/include SF.i 
+swig_cmd=swig2.0
+include=/tmp/gsl-master/include
+if [[ $HOSTNAME == "hakon-Precision-7530" ]] ; then
+    swig_cmd=swig
+    include=/usr/include
+fi
+"$swig_cmd" -w451,454 -perl5 -I"$include" SF.i 
 
 name=SF
 gcc -I${perl_include_dir} -c -fPIC -g3 -o SF_wrap.o SF_wrap.c
